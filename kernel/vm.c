270cda536c9c2cbf91faf291ac011d3a29b33031
xuzz <xuzhzhong@163.com>
2024-07-19 15:23:36 +0800

    my solution for print a page table (easy)

--- kernel/vm.c
+++ kernel/vm.c
@@ -449,3 +449,29 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+
+void
+vmprint(pagetable_t pagetable)
+{
+  static int pgtbl_level = 2;
+  
+  if(pgtbl_level == 2){
+    printf("page table %p\n", pagetable);
+  }
+
+  for(int i = 0; i < 512; i++){
+    pte_t pte = pagetable[i];
+    if(pte & PTE_V){
+      for(int j = 3 - pgtbl_level; j > 0; j--){
+        printf(" ..");
+      }
+      printf("%d: pte %p pa %p\n", i, pte, PTE2PA(pte));
+      if((pte & (PTE_R | PTE_W | PTE_X)) == 0){
+        pgtbl_level--;
+        vmprint((pagetable_t)PTE2PA(pte));
+        pgtbl_level++;
+      }
+    }
+  }
+}
